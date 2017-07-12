5d6c06faf0146be18b2e1eafb5e722e10188ae34
Simon Gomizelj <simon@vodik.xyz>
2017-07-12 12:45:05 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import codecs
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    file_path = os.path.join(os.path.dirname(__file__), fname)
+    return codecs.open(file_path, encoding='utf-8').read()
+
+
+setup(
+    name='aiosip-agent-testing',
+    version='0.0.1',
+    author='Simon Gomizelj',
+    author_email='sgomizelj@sangoma.com',
+    packages=find_packages(),
+    license='Apache 2',
+    url='https://github.com/vodik/aiosip-agent-testing',
+    description='Pure python tools for SIP testing',
+    long_description=read('README.md'),
+    install_requires=[
+        'aiosip',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Pytest',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: Apache Software License'
+    ],
+)
