5ce8c2fc2b864ccd46a5def4d664b0ae0ad79251
Simon Gomizelj <simon@vodik.xyz>
2017-07-21 10:07:06 -0400

    Fix To/From headers on response

--- useragent/__init__.py
+++ useragent/__init__.py
@@ -24,6 +24,8 @@ class Request:
     def respond(self, *args, **kwargs):
         headers = kwargs.pop('headers', {})
         headers['CSeq'] = self.data.headers['CSeq']
+        headers['To'] = self.data.headers['To']
+        headers['From'] = self.data.headers['From']
         return self.agent.send_response(*args, **kwargs, headers=headers)
 
     def __str__(self):
@@ -46,6 +48,9 @@ class Response:
     def ack(self, *args, **kwargs):
         headers = kwargs.pop('headers', {})
         cseq, _, _ = self.data.headers['CSeq'].partition(' ')
+
+        headers['To'] = self.data.headers['To']
+        headers['From'] = self.data.headers['From']
         headers['CSeq'] = f'{cseq} ACK'
         return self.agent.send_request('ACK', *args, **kwargs, headers=headers)
 
